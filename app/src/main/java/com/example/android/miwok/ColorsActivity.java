23e4ea0466dff288deba3bcba4c6727a63fd3a37
Rohit Kumar <rk15072000@gmail.com>
2021-08-21 22:54:47 +0530

    first commit-lesson2

---
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+        // Create a list of words
+        ArrayList<Word> words=new ArrayList<Word>();
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        /**Log msg to show the indexes value of arraylist
+         *
+         for(int i=0;i<words.size();i++){
+         Log.v("NumberActivity", "Words at index: "+i+" ="+words.get(i));
+         }
+         */
+
+        /**FOR LOOP
+         * Creating textView as a child of the rootView
+         * and displaying all the items from list
+         * in each textView respectively
+         //        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
+         //        for (int index=0;index <words.size();index++){
+         //            //Create a TV
+         //            TextView wordView =new TextView(this);
+         //            //Set word as text of the current index
+         //            wordView.setText(words.get(index));
+         //            //Add TV as another childView to the root View
+         //            rootView.addView(wordView);
+         //        }
+         */
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter itemAdapter =new WordAdapter (this,words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView=(ListView)findViewById(R.id.listView);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemAdapter);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+        // Create a list of words
+        ArrayList<Word> words=new ArrayList<Word>();
+        words.add(new Word("one","lutti"));
+        words.add(new Word("two","otiiko"));
+        words.add(new Word("three","tolookosu"));
+        words.add(new Word("four","oyyisa"));
+        words.add(new Word("five","massokka"));
+        words.add(new Word("six","temmokka"));
+        words.add(new Word("seven","kenekaku"));
+        words.add(new Word("eight","kawinta"));
+        words.add(new Word("nine","wo’e"));
+        words.add(new Word("ten","na’aacha"));
+
+        /**Log msg to show the indexes value of arraylist
+         *
+           for(int i=0;i<words.size();i++){
+            Log.v("NumberActivity", "Words at index: "+i+" ="+words.get(i));
+           }
+         */
+
+        /**FOR LOOP
+         * Creating textView as a child of the rootView
+         * and displaying all the items from list
+         * in each textView respectively
+//        LinearLayout rootView=(LinearLayout)findViewById(R.id.rootView);
+//        for (int index=0;index <words.size();index++){
+//            //Create a TV
+//            TextView wordView =new TextView(this);
+//            //Set word as text of the current index
+//            wordView.setText(words.get(index));
+//            //Add TV as another childView to the root View
+//            rootView.addView(wordView);
+//        }
+         */
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter itemAdapter =new WordAdapter (this,words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView=(ListView)findViewById(R.id.listView);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemAdapter);
+          
+    }
+}
